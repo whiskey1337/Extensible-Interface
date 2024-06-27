1efe1b9f04cfa28a1584f36746ffe5e8e46983ad
Andrey A. Gunko <aag131@tpu.ru>
2024-06-27 15:16:48 +0700

    Added menu open/close functionality

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup menuCG;
+    private bool isOpened = false;
+
+    private void Update()
+    {
+        if (isOpened && Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuCG.alpha = 0;
+            menuCG.interactable = false;
+            menuCG.blocksRaycasts = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        menuCG.alpha = 1;
+        menuCG.interactable = true;
+        menuCG.blocksRaycasts = true;
+        isOpened = true;
+    }
+
+
+}
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Menu : MonoBehaviour
@@ -36,7 +39,7 @@ public class Menu : MonoBehaviour
             //Секторы
             menuElements[i].menuElement.fillAmount = 1f / data.elements.Length - GapWidthDegree / 360f;
             menuElements[i].menuElement.transform.localPosition = Vector3.zero;
-            menuElements[i].menuElement.transform.localRotation = Quaternion.Euler(0, 0, -stepLength / 2f + GapWidthDegree / 2f + i * stepLength);
+            menuElements[i].menuElement.transform.localRotation = Quaternion.Euler(0, 0, stepLength / 2f + GapWidthDegree / 2f + i * stepLength);
             menuElements[i].menuElement.color = new Color(1f, 1f, 1f, 0.5f);
 
             //Иконка сектора
@@ -44,4 +47,26 @@ public class Menu : MonoBehaviour
             menuElements[i].icon.sprite = data.elements[i].Icon;
         }
     }
+
+    private void Update()
+    {
+        var stepLength = 360f / data.elements.Length;
+        var mouseAngle = NormalizedAngle(Vector3.SignedAngle(Vector3.up, Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2), Vector3.forward) + stepLength / 2f);
+        var activeElement = (int)(mouseAngle / stepLength);
+        
+
+        for (int i = 0; i < data.elements.Length; i++) 
+        {
+            if (i == activeElement)
+            {
+                menuElements[i].menuElement.color = new Color(1f, 1f, 1f, 0.75f);
+            }
+            else
+            {
+                menuElements[i].menuElement.color = new Color(1f, 1f, 1f, 0.5f);
+            }
+        }
+    }
+
+    private float NormalizedAngle(float a) => (a + 360f) % 360f;
 }
